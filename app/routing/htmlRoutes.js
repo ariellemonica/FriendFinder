541d26bcfba075a6944fba7a5d5a8ccdab8d35f1
Monica Nadal <missmonicanadal@gmail.com>
2020-05-09 14:35:22 -0700

    20200509 routing start for all moved after root routing

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -7,15 +7,14 @@ app.get("/survey", function(req, res){
     res.sendFile(path.join(__dirname, "/../public/survey.html"));
 })
 
+app.get("/", function(req, res){
+    res.sendFile(path.join(__dirname, "../public/home.html"));
+});
+
 //needs to be the last thing in the router because * means everything goes to this
 app.get("*", function(req, res){
     res.sendFile(path.join(__dirname, "../public/home.html"));
-})
-
-app.get("/", function(req, res){
-    res.json(path.join(__dirname, "../public/home.html"));
-})
-
+});
 //note - if you only have the index.html then that is the default; don't have to do the app.get
 
 module.exports = app;
