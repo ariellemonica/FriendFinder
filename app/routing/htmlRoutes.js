1551aaf3aa7376ed3cf2fbb647be7dac6f509bb8
Monica Nadal <missmonicanadal@gmail.com>
2020-05-09 14:16:10 -0700

    20200509 added root path

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -12,6 +12,10 @@ app.get("*", function(req, res){
     res.sendFile(path.join(__dirname, "../public/home.html"));
 })
 
+app.get("/", function(req, res){
+    res.sendFile(path.join(__dirname, "../public/home.html"));
+})
+
 //note - if you only have the index.html then that is the default; don't have to do the app.get
 
 module.exports = app;
