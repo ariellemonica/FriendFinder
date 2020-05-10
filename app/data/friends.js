fb882ad8e205b0504e0d0500e752bc22c723dc92
Monica Nadal <missmonicanadal@gmail.com>
2020-05-09 22:47:50 -0700

    20200509 friend data updated

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,7 +1,7 @@
 var friendNamesArr = [
     {
         "name": "Mister Anderson",
-        "photo": "https://vignette.wikia.nocookie.net/matrix/images/3/32/Neo.jpg/revision/latest/top-crop/width/200/height/150?cb=20060715235228",
+        "photo": "https://vignette.wikia.nocookie.net/matrix/images/3/32/Neo.jpg/revision/latest/scale-to-width-down/310?cb=20060715235228",
         "scores": [
             5,
             5,
@@ -17,7 +17,7 @@ var friendNamesArr = [
     },
     {
         "name": "Agent Smith",
-        "photo": "https://vignette.wikia.nocookie.net/matrix/images/4/4d/Agent-smith-the-matrix-movie-hd-wallpaper-2880x1800-4710.png/revision/latest/top-crop/width/200/height/150?cb=20140504013834",
+        "photo": "https://vignette.wikia.nocookie.net/matrix/images/4/4d/Agent-smith-the-matrix-movie-hd-wallpaper-2880x1800-4710.png/revision/latest/scale-to-width-down/310?cb=20140504013834",
         "scores": [
             1,
             1,
@@ -33,7 +33,7 @@ var friendNamesArr = [
     },
     {
         "name": "Morpheus",
-        "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        "photo": "https://vignette.wikia.nocookie.net/matrix/images/9/9b/Morpheus1.jpg/revision/latest/scale-to-width-down/266?cb=20090501203241",
         "scores": [
             5,
             4,
@@ -49,18 +49,18 @@ var friendNamesArr = [
     },
     {
         "name": "Trinity",
-        "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        "photo": "https://vignette.wikia.nocookie.net/matrix/images/6/67/Trinityfull.jpg/revision/latest/scale-to-width-down/310?cb=20060803214449",
         "scores": [
-            5,
-            5,
-            4,
             3,
-            5,
-            1,
             3,
-            2,
-            1,
-            1
+            3,
+            3,
+            3,
+            3,
+            3,
+            3,
+            3,
+            3
         ]
     },
     {
@@ -92,7 +92,7 @@ var friendNamesArr = [
             3,
             3,
             3,
-            3
+            4
         ]
     }
     //add more
